47e849cbba5fa8085e255539a7b2a33b55f132db
czert <pyhovoleg31@gmail.com>
2021-09-18 02:52:08 +0500

    warnings and errors to cout

--- src/log.hpp
+++ src/log.hpp
@@ -3,8 +3,8 @@
 #include <iostream>
 
 #define LOG_INFO(X) std::cout << "INFO: " << X << std::endl
-#define LOG_WARNING(X) std::cerr << "WARNING: " << X << std::endl
-#define LOG_ERROR(X) std::cerr << "ERROR: " << X << std::endl
+#define LOG_WARNING(X) std::cout << "WARNING: " << X << std::endl
+#define LOG_ERROR(X) std::cout << "ERROR: " << X << std::endl
 
 #if defined(_MSC_VER) && defined(_DEBUG) || defined(__GNUC__)
 #define LOG_DEBUG(X) std::cout << "DEBUG: " << X << std::endl
